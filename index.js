20cc3d561d6324918f58e7a625891325c95b63f8
Bradley Bond <bradleybond766@gmail.com>
2020-05-18 22:13:32 -0600

    exercise 3.5 completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
 	{
       "name": "Arto Hellas",
@@ -57,6 +59,15 @@ app.delete('/api/persons/:id', (req,res) =>{
 	id = Number(req.params.id)
 	persons = persons.filter(person => person.id !== id)
 	res.status(204).end()
+})
+
+app.post('/api/persons', (req,res) =>{
+	const person = req.body
+	person.id = Math.round(Math.random()*1000)
+
+	console.log(person)
+
+	res.json(person)
 })
 
 const PORT = 3001
